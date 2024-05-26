78f7996ce26b1fbb9de9761ee31040a8e84ebdef
gracebir <grace.devolop@gmail.com>
2024-05-26 06:56:36 +0200

    jwt filter config

---
+++ src/main/java/com/grace/security/config/ApplicationConfig.java
@@ -0,0 +1,4 @@
+package com.grace.security.config;
+
+public class ApplicationConfig {
+}
--- src/main/java/com/grace/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/grace/security/config/JwtAuthenticationFilter.java
@@ -1,4 +1,62 @@
 package com.grace.security.config;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        // 1 obtain header that contains jwt
+        String authHeader = request.getHeader("Authorization");
+        String jwt;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 2 obtain the jwt token
+        jwt = authHeader.split(" ")[1];
+
+        // 3 obtain subject/username in jwt
+        final String userEmail = jwtService.extractUsername(jwt);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() ==null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwt, userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                     userDetails,
+                     null,
+                     userDetails.getAuthorities()
+                );
+
+                authToken
+                        .setDetails(
+                                new WebAuthenticationDetailsSource().buildDetails(request)
+                        );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+
+            filterChain.doFilter(request, response);
+
+        }
+    }
 }
